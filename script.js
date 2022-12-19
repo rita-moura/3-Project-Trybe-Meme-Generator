495c40f473e9418e40dae740c2031a9e00681572
Rita Moura <ritamoura520@gmail.com>
2022-12-19 16:51:33 -0300

    Feat: Requisito 2
    Implementa o input que carrega a imagem e o preview dela

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function inputValue(text) {
 
 const inputText = document.getElementById('text-input');
 inputText.oninput = function input() { inputValue(this); };
+
+const inputImage = function img(event) {
+  const image = document.getElementById('meme-image');
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+const inputFile = document.getElementById('meme-insert');
+inputFile.onchange = function file(event) { inputImage(event); };
