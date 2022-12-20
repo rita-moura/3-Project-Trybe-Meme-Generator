71beb194b00a1fd8d29a54d0164f5a9dcbd19d60
Rita Moura <ritamoura520@gmail.com>
2022-12-20 09:21:05 -0300

    Feat: Requisito 7
    Implementa as fotos exemplos e refatora os demais requisitos

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
+const inputText = document.getElementById('text-input');
+const inputFile = document.getElementById('meme-insert');
+const img = document.getElementById('meme-image');
+const previewBorder = document.getElementById('meme-image-container');
+const redButton = document.getElementById('fire');
+const blueButton = document.getElementById('water');
+const greenButton = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
+
 function inputValue(text) {
   const paragraph = document.getElementById('meme-text');
   paragraph.innerText = text.value;
   return paragraph;
 }
 
-const inputText = document.getElementById('text-input');
-inputText.oninput = function input() { inputValue(this); };
-
 function inputImage(event) {
   const image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 }
-
-const inputFile = document.getElementById('meme-insert');
-inputFile.onchange = function file(event) { inputImage(event); };
-
-const previewBorder = document.getElementById('meme-image-container');
 
 function buttonRed() {
   previewBorder.style.border = 'rgb(255 0 0) dashed 3px';
   return previewBorder;
 }
 
-const redButton = document.getElementById('fire');
-redButton.onclick = function red() { buttonRed(); };
-
 function buttonBlue() {
   previewBorder.style.border = 'rgb(0 0 255) double 5px';
   return previewBorder;
 }
-
-const blueButton = document.getElementById('water');
-blueButton.onclick = function blue() { buttonBlue(); };
 
 function buttongreen() {
   previewBorder.style.border = 'rgb(0 128 0) groove 6px';
   return previewBorder;
 }
 
-const greenButton = document.getElementById('earth');
+meme1.addEventListener('click', () => {
+  img.className = 'image';
+  img.src = './imgs/meme1.png';
+});
+
+meme2.addEventListener('click', () => {
+  img.className = 'image';
+  img.src = './imgs/meme2.png';
+});
+
+meme3.addEventListener('click', () => {
+  img.className = 'image';
+  img.src = './imgs/meme3.png';
+});
+
+meme4.addEventListener('click', () => {
+  img.className = 'image';
+  img.src = './imgs/meme4.png';
+});
+
+inputText.oninput = function input() { inputValue(this); };
+inputFile.onchange = function file(event) { inputImage(event); };
 greenButton.onclick = function green() { buttongreen(); };
+blueButton.onclick = function blue() { buttonBlue(); };
+redButton.onclick = function red() { buttonRed(); };
